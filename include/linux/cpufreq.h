eb6c38bd41aa0827d0cae48f00e8eb3a6f33afa6
YoshiShaPow <yoshipga@gmail.com>
2014-07-17 14:13:27 -0700

    Trying to make intelliactive default

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -371,7 +371,7 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_ondemandplus)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
